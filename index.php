0e7158de026177c7b12cd3ac5a104a80c2ebc87f
Efrungxx69 <102815232+Efrungxx69@users.noreply.github.com>
2023-10-06 09:39:58 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>
+        Personal Portfolio
+    </title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+
+<body>
+
+    <header class="header">
+        <a href="index.html" class="logo">FRUNG.</a>
+
+        <nav class="navbar">
+            <a href="#" class="active">Home</a>
+            <a href="#">About</a>
+            <a href="#">Svericss</a>
+            <a href="#">Portfolio</a>
+            <a href="#">Contact</a>
+        </nav>
+    </header>
+
+    <section class="home">
+        <div class="home-content">
+            <h1>Hi, I'm Frung</h1>
+            <h3>Back end Developer</h3>
+            <p>  Dedicated Computer Engineer student with
+                 a strong command of Python and proficiency 
+                 in Visual Studio Code, seeking a role as an 
+                 entry-level software developer. Eager to bring 
+                 my ability to develop, test, and maintain software 
+            </p>
+            <div class="btn-box">
+                <a href="#">Hire Me</a>
+                <a href="#">Let's Talk</a>
+            </div>  
+        </div>
+
+        <div class="home-sci">
+            <a href="https://www.facebook.com/frung.frung.5"><i class='bx bxl-facebook' ></i></a>
+            <a href="https://www.instagram.com/frungphia/"><i class='bx bxl-instagram' ></i></a>
+            <a href="#"><i class='bx bx-link' ></i></a>
+        </div>
+
+        <span class="home-imgHover"></span>
+
+    </section>
+</body>
+
+</html>
+
